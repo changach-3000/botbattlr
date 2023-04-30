4de30055b4683ac4bcae74f5de49f798e09020ec
changach-3000 <sharon.changach@student.moringaschool.com>
2023-04-30 10:34:01 +0300

    Updated collection

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -1,6 +1,8 @@
 import React,{useState,useEffect} from 'react'
+import SingleBot from './SingleBot'
+import {Link} from 'react-router-dom'
 
-function BotCollection({bots, setArmyHandler,handleDelete}) {
+function BotCollection({bots,handleDelete,setArmyHandler}) {
   return (
 
     <div className=' container my-5'>
@@ -8,11 +10,13 @@ function BotCollection({bots, setArmyHandler,handleDelete}) {
      {/* render the bots in cards */}
     <div className=" row ">
         {
-          bots.map((bot)=>(
+          bots.map((bot)=>( 
           <div key={bot.id} className="col-3 mb-4  mr-3 px-3">
           <div className='border bg-light overflow-hidden'  >
+          <Link to={`bot/${bot.id}`}>
             <img src="https://img.icons8.com/tiny-color/16/null/close-window.png" onClick={()=> handleDelete(bot.id)}/>
-            <img src={bot.avatar_url} className='img-fluid ' alt='image loading...' onClick={() => setArmyHandler(bot)}/>
+            <img src={bot.avatar_url} className='img-fluid ' alt='image loading...' />
+            </Link>
             <h5 style={{textAlign:'center',fontWeight:'bolder'}}>{bot.name}</h5>
             <p>{bot.catchphrase}</p>
             {/* style the bot details */}
