bf684bcfbcd87d50131a74c22c8d262ef468a054
changach-3000 <sharon.changach@student.moringaschool.com>
2023-04-30 12:57:32 +0300

    Add Back button

--- src/components/SingleBot.jsx
+++ src/components/SingleBot.jsx
@@ -2,6 +2,8 @@ import React,{useState,useEffect} from 'react'
 import { useParams,useNavigate} from "react-router-dom";
 
 
+
+
 function SingleBot({handleDelete,setArmyHandler}) {
     const [singlebot,setSingleBot] =useState(" ")
 
@@ -15,25 +17,28 @@ function SingleBot({handleDelete,setArmyHandler}) {
        
       })
     }, [])
-  
+    const goBack = () => {
+      nav(-1);}
   return (
+   
     <div>
         {
-            
-          <div key={singlebot.id} className="col-3 mb-4  mr-3 px-3">
+          <div key={singlebot.id} className="col-3 mb-5 mr-3 px-5 mt-5 ">
             <div className='border bg-light overflow-hidden'  >
             <img src="https://img.icons8.com/tiny-color/16/null/close-window.png" onClick={()=> handleDelete(singlebot.id)}/>
             <img src={singlebot.avatar_url} className='img-fluid ' alt='image loading...'/>
             <h5 style={{textAlign:'center',fontWeight:'bolder'}}>{singlebot.name}</h5>
-            <p>{singlebot.catchphrase}</p>
+            <p>CatchPhrase :{singlebot.catchphrase}</p>
+            <p>Class: {singlebot.bot_class}</p>
             {/* style the bot details */}
             <hr />
             <div id="bot-details">
-            <p>H:{singlebot.health}</p>
-            <p>D:{singlebot.damage}</p>
-            <p>A:{singlebot.armor}</p>
+            <p><i class="heartbeat icon"></i>{singlebot.health}</p>
+            <p><i class="icon lightning"></i>{singlebot.damage}</p>
+            <p><i class="icon shield"></i>{singlebot.armor}</p>
           </div>
           <button onClick={() => setArmyHandler(singlebot)}>Add to Army</button>
+          <button onClick={goBack}>Go back</button>
           </div> 
           </div> 
          
