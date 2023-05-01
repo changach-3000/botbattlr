744dcee568521454138631cd52885b88c0e12bf7
changach-3000 <sharon.changach@student.moringaschool.com>
2023-05-01 15:34:00 +0300

    Added sort functionality to health

--- src/components/SortBar.js
+++ src/components/SortBar.js
@@ -1,18 +1,18 @@
-import React ,{useState} from 'react'
+import React,{useState} from 'react';
 
-function SortBar({bots,setBots}) {
-  const [sorted, setSorted] = useState([]);
-  
-const handleSort = () => {
-sorted = bots.sort((a, b) => (
-  a.name.localeCompare(b.name))
-)
-setSorted(sorted);
-setBots(setSorted)
-}
+function SortBar({ bots, setBots }) {
+  const [sorted, setSorted]=useState([])
+ 
+  const sortBotsByHealth = () => {
+    const sortedBots = bots.sort((a, b) => a.health - b.health);
+    setBots(sortedBots);
+    setSorted(sortedBots);
+  };
   return (
-    <div>
-    </div>
+    <div className="sort-bar">
+    <button id="sort-btn" onClick={sortBotsByHealth}>Sort by Health</button>
+       </div>
   );
-  }
-export default SortBar
+}
+
+export default SortBar;
